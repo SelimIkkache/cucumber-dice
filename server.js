5e843cd5220c375593816745eddf1a3f9e2d4294
SelimIkkache <selim.ikkache@gmail.com>
2017-11-25 16:13:44 +0100

    Arrange the html output

--- server.js
+++ server.js
@@ -16,15 +16,14 @@ app.use((request, response, next) => {
 app.use((request, response, next) => {
   var Dice = require("./app/dice.js")
   var dice = new Dice(6)
-  request.chance = dice.jet()
+  request.chance = dice.jet().toString()
+  console.log("Jet : " + request.chance)
   next()
 })
 
 // Manage the route
 app.get('/', (request, response) => {
-  response.json({
-    chance: request.chance
-  })
+  response.send("<p>" + request.chance + "</p>")
 })
 
 app.use((err, request, response, next) => {
